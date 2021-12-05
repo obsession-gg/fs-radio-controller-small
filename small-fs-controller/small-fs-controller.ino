e19b58e5e42da68afe1c5cfc725cf74257ad2707
obsession-gg <94297259+obsession-gg@users.noreply.github.com>
2021-12-05 08:11:44 -0500

    Add files via upload

--- small-fs-controller/small-fs-controller.ino
+++ small-fs-controller/small-fs-controller.ino
@@ -1,22 +1,28 @@
-#define SERIAL_DATA 2
-#define SERIAL_CLOCK 3
-#define ONBOARD_LED 13
-#define pushButton1 23
+#define rotary1B      13      //rotary1 counter-clockwise, B - C connect first          
+#define rotary1A      12      //rotary1 clockwise, A - C connect first
+#define pushButton1   23      //pushbutton for rotary1
+
+#define ONBOARD_LED   13
+
+byte rotary1Home = 0;
 
 void setup() {
 
 pinMode(ONBOARD_LED, OUTPUT);
-pinMode(SERIAL_DATA, INPUT);
-pinMode(SERIAL_CLOCK, INPUT);
+pinMode(rotary1A, INPUT);
+pinMode(rotary1B, INPUT);
 pinMode(pushButton1, INPUT);
 
 }
 
 void loop() {
 
-if(digitalRead(pushButton1) == LOW){
-  digitalWrite(ONBOARD_LED, HIGH);
 }
-else digitalWrite(ONBOARD_LED, LOW);
 
+//with the encoder, I want to be able to recognize which direction I'm turning the dial.
+//the direction I turn the dial will increment a parameter up or down. 
+
+bool rotary1Direction (){
+  byte initPos = 0;
+  //byte
 }
